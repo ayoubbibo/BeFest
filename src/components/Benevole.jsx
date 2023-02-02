a8bf82f597a471ebe4a83d2f1feb517b748ad383
hakemi <hakemiayoub612@gmail.com>
2023-02-02 22:44:04 +0100

    List Benevols

--- src/components/Benevole.jsx
+++ src/components/Benevole.jsx
@@ -1,5 +1,6 @@
 import '../styles/Benevole.css';
 import Header from './Header';
+import BenevoleCard from './BenevoleCard';
 import { Form, Button } from 'react-bootstrap';
 //import Axios from "axios";
 //import { useEffect, useState } from 'react';
@@ -29,23 +30,67 @@ const clotheType = ['BlazerShirt', 'BlazerSweater', 'CollarSweater', 'GraphicShi
 const clotheColor = ['Black', 'Blue01', 'Blue02', 'Blue03', 'Gray01', 'Gray02', 'Heather', 'PastelBlue', 'PastelGreen', 'PastelOrange', 'PastelRed', 'PastelYellow', 'Pink', 'Red', 'White'];
 const eyeType = ['Close', 'Cry', 'Default', 'Dizzy', 'EyeRoll', 'Happy', 'Hearts', 'Side', 'Squint', 'Surprised', 'Wink', 'WinkWacky'];
 const eyebrowType = ['Angry', 'AngryNatural', 'Default', 'DefaultNatural', 'FlatNatural', 'RaisedExcited', 'RaisedExcitedNatural', 'SadConcerned', 'SadConcernedNatural', 'UnibrowNatural', 'UpDown', 'UpDownNatural'];
-const mouthType = ['Concerned', 'Default', 'Disbelief', 'Eating', 'Grimace', 'Sad', 'ScreamOpen', 'Serious', 'Smile', 'Tongue', 'Twinkle', 'Vomit'];
-const skinColor = ['Tanned', 'Yellow', 'Pale', 'Light', 'Brown', 'DarkBrown', 'Black'];
 
-
-const avatarStyle = 'Circle';
-const topType1 = topType[Math.floor(Math.random() * topType.length)];
-const accessoriesType1 = accessoriesType[Math.floor(Math.random() * accessoriesType.length)];
-const hairColor1 = hairColor[Math.floor(Math.random() * hairColor.length)];
-const facialHairType1 = facialHairType[Math.floor(Math.random() * facialHairType.length)];
-const facialHairColor1 = facialHairColor[Math.floor(Math.random() * facialHairColor.length)];
-const clotheType1 = clotheType[Math.floor(Math.random() * clotheType.length)];
-const clotheColor1 = clotheColor[Math.floor(Math.random() * clotheColor.length)];
-const eyeType1 = eyeType[Math.floor(Math.random() * eyeType.length)];
-const eyebrowType1 = eyebrowType[Math.floor(Math.random() * eyebrowType.length)]; 
+// Create a function to generate random avatar
+function randomAvatar() {
+    const topType1 = topType[Math.floor(Math.random() * topType.length)];
+    const accessoriesType1 = accessoriesType[Math.floor(Math.random() * accessoriesType.length)];
+    const hairColor1 = hairColor[Math.floor(Math.random() * hairColor.length)];
+    const facialHairType1 = facialHairType[Math.floor(Math.random() * facialHairType.length)];
+    const facialHairColor1 = facialHairColor[Math.floor(Math.random() * facialHairColor.length)];
+    const clotheType1 = clotheType[Math.floor(Math.random() * clotheType.length)];
+    const clotheColor1 = clotheColor[Math.floor(Math.random() * clotheColor.length)];
+    const eyeType1 = eyeType[Math.floor(Math.random() * eyeType.length)];
+    const eyebrowType1 = eyebrowType[Math.floor(Math.random() * eyebrowType.length)];
+    const avatarUrl = `https://avataaars.io/?avatarStyle=Circle&topType=${topType1}&accessoriesType=${accessoriesType1}&hairColor=${hairColor1}&facialHairType=${facialHairType1}&facialHairColor=${facialHairColor1}&clotheType=${clotheType1}&clotheColor=${clotheColor1}&eyeType=${eyeType1}&eyebrowType=${eyebrowType1}`;
+    return avatarUrl;
+}
 
 // create the url for the avatar
-const avatarUrl = `https://avataaars.io/?avatarStyle=${avatarStyle}&topType=${topType1}&accessoriesType=${accessoriesType1}&hairColor=${hairColor1}&facialHairType=${facialHairType1}&facialHairColor=${facialHairColor1}&clotheType=${clotheType1}&clotheColor=${clotheColor1}&eyeType=${eyeType1}&eyebrowType=${eyebrowType1}`;
+const avatarUrl = randomAvatar();
+
+// Create a list that will contains a list of volunteers
+const list = [
+    {
+        id: 1,
+        nom: "Ben",
+        prenom: "Moussa",
+        email: "ben.moussa@gmail.com"
+    },
+    {
+        id: 2,
+        nom: "Ben",
+        prenom: "Moussa",
+        email: "ben.moussa@gmail.com"
+    },
+    {
+        id: 2,
+        nom: "Ben",
+        prenom: "Moussa",
+        email: "ben.moussa@gmail.com"
+    },
+    {
+        id: 2,
+        nom: "Ben",
+        prenom: "Moussa",
+        email: "ben.moussa@gmail.com"
+    }
+    ,
+    {
+        id: 2,
+        nom: "Ben",
+        prenom: "Moussa",
+        email: "ben.moussa@gmail.com"
+    }
+    ,
+    {
+        id: 2,
+        nom: "Ben",
+        prenom: "Moussa",
+        email: "ben.moussa@gmail.com"
+    }
+]
+
 
 return(
         <div className="app">
@@ -56,7 +101,6 @@ return(
                         <Form.Group className="mb-3">
                             <img src={avatarUrl} className="avatar"/>                                
                         </Form.Group>
-                        
                         <Form.Group className="mb-3" >
                             <Form.Control type="Text" placeholder="Entrer Le Nom" />
                         </Form.Group>
@@ -71,6 +115,17 @@ return(
                             Ajouter Le Bénévole
                         </Button>
                     </Form>
+                </div>
+
+                <div className="liste_ben_container">
+                    <ul className="ben_cards">
+                        {
+                            list.map((item) => (
+                                <BenevoleCard key={item.id} nom={item.nom} prenom={item.prenom} avatar={avatarUrl}> 
+                                </BenevoleCard>
+                            ))
+                        }   
+                    </ul>
                 </div>
             </div>
         </div>
