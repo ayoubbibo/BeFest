e733b82e59b9fea10e34bc885399406da0730dcb
HAKEMI Ayoub <ayoub.hakemi@igtp3-pc12.isim.intra>
2023-02-09 16:06:32 +0100

    drag and drop Work

--- src/components/Benevole.jsx
+++ src/components/Benevole.jsx
@@ -1,9 +1,10 @@
 import '../styles/Benevole.css';
+import '../styles/BenevoleCard.css';
 import Header from './Header';
-import BenevoleCard from './BenevoleCard';
 import { Form, Button } from 'react-bootstrap';
-import React, { useState, useRef } from 'react';
+import React, { useState} from 'react';
 import {Card} from 'react-bootstrap';
+import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 
 //import Axios from "axios";
 //import { useEffect, useState } from 'react';
@@ -64,8 +65,8 @@ const [list, setList] = useState(
     },
     {
         id: 2,
-        nom: "Ben",
-        prenom: "Moussa",
+        nom: "bibo",
+        prenom: "ahmed",
         email: "ben.moussa@gmail.com"
     },
     {
@@ -83,31 +84,23 @@ const [list, setList] = useState(
 ]
 )
 
+const [zones, setZones] = useState([]);
 
-const dragItem = useRef();
-const dragOverItem = useRef();
+function handleOnDragEnd(result){
+    console.log(result);
+    if(!result.destination) return;
+    const items = Array.from(list);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setList(items);
 
-const dragStart = (e, position) => {
-  dragItem.current = position;
-  console.log(e.target.innerHTML);
-};
-
-const dragEnter = (e, position) => {
-  dragOverItem.current = position;
-  console.log(e.target.innerHTML);
-};
-
-const drop = (e) => {
-    const copyListItems = [...list];
-    const dragItemContent = copyListItems[dragItem.current];
-    copyListItems.splice(dragItem.current, 1);
-    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-    dragItem.current = null;
-    dragOverItem.current = null;
-    // drop the li item in the cont div
-    e.target.appendChild(e.target.childNodes[0]);
-    setList(copyListItems);
-  };
+    if (result.destination.droppableId === "zones") {
+        // push the item to the zones
+        const zonesList = Array.from(zones);
+        zonesList.push(reorderedItem);
+        setZones(zonesList);
+    }
+}
 
 return(
         <div className="app">
@@ -116,7 +109,7 @@ return(
                 <div className="ajoute_ben_container">
                     <Form className="form_ajout">
                         <Form.Group className="mb-3">
-                            <img src={avatarUrl} className="avatar"/>                                
+                            <img src={avatarUrl} className="avatar" alt="the avatar"/>                                
                         </Form.Group>
                         <Form.Group className="mb-3" >
                             <Form.Control type="Text" placeholder="Entrer Le Nom" />
@@ -135,33 +128,67 @@ return(
                 </div>
 
                 <div className="liste_ben_container">
-                    <ul className="ben_cards">
-                        {
-                            list.map((item, index) => (
-                                    <li key={index}  
-                                        onDragStart={(e) => dragStart(e, index)}
-                                        onDragEnter={(e) => dragEnter(e, index)} 
-                                        onDragEnd={drop}
-                                        draggable>
-                                    <Card className="ben-li">
-                                        <Card.Body className="ben-li-body">
-                                            <Card.Img variant="top" src={avatarUrl} className="ben-cover__item" alt="Logo"/>
-                                            <div>
-                                                <Card.Title>{item.nom}<br/>
-                                                </Card.Title>
-                                                <Card.Text>
-                                                    {item.prenom}	
-                                                </Card.Text>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                </li>                    
-                            ))
-                        }   
-                    </ul>
-                </div>
-                <div className="cont">
-                
+                    <DragDropContext onDragEnd={handleOnDragEnd}>    
+                        <Droppable droppableId="benev">
+                            {(provided) => (
+                                <ul className="ben_cards" {...provided.droppableProps} ref={provided.innerRef}>
+                                {
+                                    list.map((item, index) => (
+                                        <Draggable key={item.id} draggableId={item.id+item.nom} index={index}>    
+                                            {(provided) => (
+                                                <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                                                    <Card className="ben-li">
+                                                        <Card.Body className="ben-li-body">
+                                                            <Card.Img variant="top" src={avatarUrl} className="ben-cover__item" alt="The image is clear"/>
+                                                            <div>
+                                                                <Card.Title>{item.nom}<br/>
+                                                                </Card.Title>
+                                                                <Card.Text>
+                                                                    {item.prenom}	
+                                                                </Card.Text>
+                                                            </div>
+                                                        </Card.Body>
+                                                    </Card>
+                                                </li>
+                                            )}
+                                        </Draggable>
+                                    ))
+                                }
+                                {provided.placeholder}
+                                </ul>
+                            )}
+                        </Droppable>
+                        <Droppable  droppableId="zones" className="cont">
+                            {(provided) => (
+                                <ul className="cont" {...provided.droppableProps} ref={provided.innerRef}>
+                                {
+                                    zones.map((item, index) => (
+                                        <Draggable key={item.id} draggableId={item.id+item.nom} index={index}>    
+                                            {(provided) => (
+                                                <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                                                    <Card className="ben-li">
+                                                        <Card.Body className="ben-li-body">
+                                                            <Card.Img variant="top" src={avatarUrl} className="ben-cover__item" alt="The image is clear"/>
+                                                            <div>
+                                                                <Card.Title>{item.nom}<br/>
+                                                                </Card.Title>
+                                                                <Card.Text>
+                                                                    {item.prenom}	
+                                                                </Card.Text>
+                                                            </div>
+                                                        </Card.Body>
+                                                    </Card>
+                                                </li>
+                                            )}
+                                        </Draggable>
+                                    ))
+                                }
+                                {provided.placeholder}
+                                </ul>
+                            )}
+                            
+                        </Droppable>
+                    </DragDropContext>
                 </div>
             </div>
         </div>
