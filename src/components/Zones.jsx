de5fc3d1c7c859d1727d28d3525cd8ee9b5ec5ad
HAKEMI Ayoub <ayoub.hakemi@igtp3-pc12.isim.intra>
2023-02-10 17:25:56 +0100

    upload

--- src/components/Zones.jsx
+++ src/components/Zones.jsx
@@ -1,8 +1,11 @@
 import '../styles/Zones.css';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './Header';
+import ZoneDetails from './ZoneDetails';
 import Axios from "axios";
 import { useEffect, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
 
 function Zones(){
 
@@ -21,17 +24,62 @@ const getZones = () => {
     .catch(err => console.log(err));
 }
 
+
+const [newZoneName, setNewZoneName] = useState('');
+
+function addZone(event){
+    event.preventDefault();
+    Axios.post(`${process.env.REACT_APP_API_URL}/zones`, {
+        name: newZoneName
+    })
+    .then(res => {
+        console.log(res.data);
+        toast.success('The Zone Is Created Succesfully!', {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        getZones();
+    })
+    .catch(err => console.log(err));
+    setNewZoneName('');
+}
+
+
+const [zoneClicked, setZoneClicked] = useState(false);
+const [zoneToDetail, setZoneToDetail] = useState({});
+
+function showZoneDetails(zone){
+    setZoneClicked(true);
+    setZoneToDetail(zone);
+}
+
 return(
         <div className="app">
             <Header/>
+            {
+                zoneClicked ? <ZoneDetails zone={zoneToDetail} /> : null
+            }
             <div className="zones-container">
                     
                 <div className="ajoute_zone_container">
                     <Form className="form_ajout">
                         <Form.Group className="mb-3">
-                            <Form.Control type="Text" placeholder="Entrer Le Nom" className='zone-input-name'/>
+                            <Form.Control 
+                                type="Text" 
+                                placeholder="Entrer Le Nom" 
+                                className='zone-input-name'
+                                value={newZoneName}
+                                onChange={(event) => { setNewZoneName(event.target.value)}}
+                            />
+                            <ToastContainer />
                         </Form.Group>
-                        <Button variant="outline-success" type="submit">
+                        <Button variant="outline-success" type="submit" onClick={addZone}>
                             Ajouter La Zone
                         </Button>
                     </Form>
@@ -42,9 +90,9 @@ return(
                         {
                             data.map((zone) => (
                                 <li key={zone._id} className="zone-li-info">
-                                    <div className="zone-name">
+                                    <button className="zone-name"  onClick={() => showZoneDetails(zone)}>
                                         {zone.name}
-                                    </div>
+                                    </button>
                                     <div className="zone-jeux">
                                         <ul className="zone-jeux-list">
                                             {
