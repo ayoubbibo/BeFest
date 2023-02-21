4db0d6922d46330b1e02a7219104d1684ee2161c
HAKEMI Ayoub <ayoub.hakemi@igtp3-pc12.isim.intra>
2023-02-21 14:30:45 +0100

    trying to advance

--- src/components/ValidateUpdate.jsx
+++ src/components/ValidateUpdate.jsx
@@ -2,8 +2,18 @@ import './../styles/ValidateUpdate.css';
 
 function ValidateUpdate() {
   return (
-    <div className="card">
-        
+    <div className="validate-update">
+      <div className="validate-update-container">
+        <div className="validate-update-content">
+          <div className="validate-update-header">
+            <h3>Are You Sure You Want To Delete This Zone?</h3>
+          </div>
+          <div className="validate-update-buttons">
+            <button className="validate-update-button">Yes</button>
+            <button className="validate-update-button">No</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ZoneNameOp.jsx
+++ src/components/ZoneNameOp.jsx
@@ -5,16 +5,32 @@ import { faPen, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 import './../styles/ZoneNameOp.css';
 import Axios from 'axios';
 import {toast } from 'react-toastify';
+import ValidateUpdate from './ValidateUpdate';
 
 const ZoneNameOp = ({ zone, setZoneToDetail, setZoneClicked, index, setData, data}) => {
   const [zoneName, setZoneName] = useState(
     zone.name
   );
 
+  const [updateValidated, setUpdateValidated] = useState(
+    false
+  );
+
+  const [contentChanged, setcontentChanged] = useState(
+    false
+  );
+
   const inputRef = React.useRef(null);
 
   useEffect(() => {
     inputRef.current.onblur = () => {
+    }
+  }, [zone, zoneName,setZoneToDetail,data,index,setData]);
+
+
+  function tryUpdate() {
+    if (contentChanged) {
+      setUpdateValidated(true);
       const reqObj = { ...zone, name: zoneName };
       setZoneToDetail(reqObj);
       setData(data.map((zone, i) => (i === index ? reqObj : zone)));
@@ -22,27 +38,25 @@ const ZoneNameOp = ({ zone, setZoneToDetail, setZoneClicked, index, setData, dat
       Axios.put(`${process.env.REACT_APP_API_URL}/zones/${zone._id}`, 
         reqObj
       )
-      .then(res => {
-        console.log(res.data);
-        toast.success('The Zone Is Updated Succesfully!', {
-          position: "top-center",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      }  
-    )
-    .catch(err => console.log(err));
+      .then(res => 
+        {
+          console.log(res.data);
+          toast.success('The Zone Is Updated Succesfully!', {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }  
+      )
+      .catch(err => console.log(err));
     }
-  }, [zone, zoneName,setZoneToDetail,data,index,setData]);
-
-
-  function tryUpdate() {
-    setZoneClicked(false);
-    inputRef.current.focus();
+    else {
+      inputRef.current.focus();
+    }
   }
 
   function tryDelete() {
@@ -67,11 +81,23 @@ const ZoneNameOp = ({ zone, setZoneToDetail, setZoneClicked, index, setData, dat
 
   return (
     <div className="zone-name-op">
+      {
+        updateValidated ? 
+            <div className="zone-details">
+                <ValidateUpdate /> 
+            </div>
+        : null
+      }
       <InputGroup>
         <Form.Control
           placeholder={zoneName}
           value={zoneName}
-          onChange={(e) => setZoneName(e.target.value)}
+          onChange={(e) => 
+            {
+              setcontentChanged(true);
+              setZoneName(e.target.value);
+            }  
+          }
           ref={inputRef}
           className="zone-name-input"
         />
