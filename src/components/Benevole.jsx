a729dc10123ffe2636d909c648f7b8767940ec8a
hakemi <hakemiayoub612@gmail.com>
2023-03-05 21:38:39 +0100

    testing the calendar on the Benevole

--- src/components/Benevole.jsx
+++ src/components/Benevole.jsx
@@ -1,157 +1,87 @@
-import '../styles/Benevole.css';
-import '../styles/BenevoleCard.css';
-import Header from './Header';
-import { Form, Button } from 'react-bootstrap';
-import React, { useState} from 'react';
-import {Card} from 'react-bootstrap';
-import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
-import { randomAvatar } from './AvatarGenerator';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  WeekView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
 
-function Benevole(){
 
-// create the url for the avatar
-const avatarUrl = randomAvatar();
+const PREFIX = 'Demo';
 
-// Create a list that will contains a list of volunteers 
-const [list, setList] = useState(
-[
-    {
-        id: 1,
-        nom: "Ben",
-        prenom: "Moussa",
-        email: "ben.moussa@gmail.com"
+const classes = {
+  todayCell: `${PREFIX}-todayCell`,
+  weekendCell: `${PREFIX}-weekendCell`,
+  today: `${PREFIX}-today`,
+  weekend: `${PREFIX}-weekend`,
+};
+
+const StyledWeekViewTimeTableCell = styled(WeekView.TimeTableCell)(({ theme }) => ({
+  [`&.${classes.todayCell}`]: {
+    backgroundColor: alpha(theme.palette.primary.main, 0.1),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.primary.main, 0.14),
     },
-    {
-        id: 2,
-        nom: "bibo",
-        prenom: "ahmed",
-        email: "ben.moussa@gmail.com"
+    '&:focus': {
+      backgroundColor: alpha(theme.palette.primary.main, 0.16),
     },
-    {
-        id: 3,
-        nom: "Ben",
-        prenom: "Moussa",
-        email: "ben.moussa@gmail.com"
+  },
+  [`&.${classes.weekendCell}`]: {
+    backgroundColor: alpha(theme.palette.action.disabledBackground, 0.04),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.action.disabledBackground, 0.04),
     },
-    {
-        id: 4,
-        nom: "Ben",
-        prenom: "Moussa",
-        email: "ben.moussa@gmail.com"
-    }
-]
-)
+    '&:focus': {
+      backgroundColor: alpha(theme.palette.action.disabledBackground, 0.04),
+    },
+  },
+}));
 
-const [zones, setZones] = useState([]);
+const StyledWeekViewDayScaleCell = styled(WeekView.DayScaleCell)(({ theme }) => ({
+  [`&.${classes.today}`]: {
+    backgroundColor: alpha(theme.palette.primary.main, 0.16),
+  },
+  [`&.${classes.weekend}`]: {
+    backgroundColor: alpha(theme.palette.action.disabledBackground, 0.06),
+  },
+}));
 
-function handleOnDragEnd(result){
-    console.log(result);
-    if(!result.destination) return;
-    const items = Array.from(list);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    setList(items);
+const TimeTableCell = (props) => {
+  const { startDate } = props;
+  const date = new Date(startDate);
 
-    if (result.destination.droppableId === "zones") {
-        // push the item to the zones
-        const zonesList = Array.from(zones);
-        zonesList.push(reorderedItem);
-        setZones(zonesList);
-    }
-}
+  if (date.getDate() === new Date().getDate()) {
+    return <StyledWeekViewTimeTableCell {...props} className={classes.todayCell} />;
+  } if (date.getDay() === 0 || date.getDay() === 6) {
+    return <StyledWeekViewTimeTableCell {...props} className={classes.weekendCell} />;
+  } return <StyledWeekViewTimeTableCell {...props} />;
+};
 
-return(
-        <div className="app">
-            <Header></Header>
-            <div className="ben-content">
-                <div className="ajoute_ben_container">
-                    <Form className="form_ajout">
-                        <Form.Group className="mb-3">
-                            <img src={avatarUrl} className="avatar" alt="the avatar"/>                                
-                        </Form.Group>
-                        <Form.Group className="mb-3" >
-                            <Form.Control type="Text" placeholder="Entrer Le Nom" />
-                        </Form.Group>
+const DayScaleCell = (props) => {
+  const { startDate, today } = props;
 
-                        <Form.Group className="mb-3">
-                            <Form.Control type="Text" placeholder="Entrer Le Prénom" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Control type="email" placeholder="Entrer L'Email" />
-                        </Form.Group>
-                        <Button variant="outline-success" type="submit">
-                            Ajouter Le Bénévole
-                        </Button>
-                    </Form>
-                </div>
+  if (today) {
+    return <StyledWeekViewDayScaleCell {...props} className={classes.today} />;
+  } if (startDate.getDay() === 0 || startDate.getDay() === 6) {
+    return <StyledWeekViewDayScaleCell {...props} className={classes.weekend} />;
+  } return <StyledWeekViewDayScaleCell {...props} />;
+};
 
-                <div className="liste_ben_container">
-                    <DragDropContext onDragEnd={handleOnDragEnd}>    
-                        <Droppable droppableId="benev">
-                            {(provided) => (
-                                <ul className="ben_cards" {...provided.droppableProps} ref={provided.innerRef}>
-                                {
-                                    list.map((item, index) => (
-                                        <Draggable key={item.id} draggableId={item.id+item.nom} index={index}>    
-                                            {(provided) => (
-                                                <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                                    <Card className="ben-li">
-                                                        <Card.Body className="ben-li-body">
-                                                            <Card.Img variant="top" src={avatarUrl} className="ben-cover__item" alt="The image is clear"/>
-                                                            <div>
-                                                                <Card.Title>{item.nom}<br/>
-                                                                </Card.Title>
-                                                                <Card.Text>
-                                                                    {item.prenom}	
-                                                                </Card.Text>
-                                                            </div>
-                                                        </Card.Body>
-                                                    </Card>
-                                                </li>
-                                            )}
-                                        </Draggable>
-                                    ))
-                                }
-                                {provided.placeholder}
-                                </ul>
-                            )}
-                        </Droppable>
-                        <Droppable  droppableId="zones" className="cont">
-                            {(provided) => (
-                                <ul className="cont" {...provided.droppableProps} ref={provided.innerRef}>
-                                {
-                                    zones.map((item, index) => (
-                                        <Draggable key={item.id} draggableId={item.id+item.nom} index={index}>    
-                                            {(provided) => (
-                                                <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                                    <Card className="ben-li">
-                                                        <Card.Body className="ben-li-body">
-                                                            <Card.Img variant="top" src={avatarUrl} className="ben-cover__item" alt="The image is clear"/>
-                                                            <div>
-                                                                <Card.Title>{item.nom}<br/>
-                                                                </Card.Title>
-                                                                <Card.Text>
-                                                                    {item.prenom}	
-                                                                </Card.Text>
-                                                            </div>
-                                                        </Card.Body>
-                                                    </Card>
-                                                </li>
-                                            )}
-                                        </Draggable>
-                                    ))
-                                }
-                                {provided.placeholder}
-                                </ul>
-                            )}
-                            
-                        </Droppable>
-                    </DragDropContext>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Benevole;
-  
+export default () => (
+  <Paper>
+    <Scheduler
+      height={660}
+    >
+      <ViewState />
+      <WeekView
+        startDayHour={9}
+        endDayHour={19}
+        timeTableCellComponent={TimeTableCell}
+        dayScaleCellComponent={DayScaleCell}
+      />
+      <Appointments />
+    </Scheduler>
+  </Paper>
+);
